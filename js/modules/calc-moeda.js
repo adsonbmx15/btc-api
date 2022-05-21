3cf07efd0eacff6dd5a5c3b10176c59a260b5cc9
adsonbmx15 <adsonbmx18@gmail.com>
2022-05-20 21:43:55 -0300

    calc ajust

--- js/modules/calc-moeda.js
+++ js/modules/calc-moeda.js
@@ -1,35 +1,42 @@
 export default function initCalcMoeda() {}
 
-const valor = document.querySelector(".form-valor input");
-const valorName = document.querySelector("#name");
-const valorSelect = document.querySelectorAll(".form-valor select");
-let valorCompra = document.querySelector(".btc-valor-compra");
-valor.addEventListener("change", handleChange);
-valorSelect.forEach((item) => {
-  item.addEventListener("change", handleChangeSelect);
-});
+fetch("https://blockchain.info/ticker")
+  .then((r) => r.json())
+  .then((bitcontValor) => {
+    const valor = document.querySelector(".form-valor input");
+    const valorName = document.querySelector("#name");
+    const valorSelect = document.querySelectorAll(".form-valor select");
+    let valorCompra = document.querySelector(".btc-valor-compra");
+    valor.addEventListener("change", handleChange);
+    valorSelect.forEach((item) => {
+      item.addEventListener("change", handleChangeSelect);
+    });
 
-let bitcont = 10000;
+    console.log(bitcontValor.BRL.symbol);
 
-function handleChange(event) {
-  const valorMoeda = event.target.value;
-  const valorFinal = valorMoeda / bitcont;
-  valorCompra.innerHTML = `<span data-simbolo>$</span>${valorFinal}`;
-}
+    let bitcont = bitcontValor.USD.sell;
+    valorCompra.innerHTML = `<span data-simbolo>$</span>${bitcont}`;
 
-async function handleChangeSelect(event) {
-  const valorMoedaSelect = event.target.value;
-  const valorInputSelect = +valorName.value;
-  function valorFinal() {
-    if (valorMoedaSelect === "1") {
-      bitcont = (valorInputSelect / 100000).toFixed(6);
-    } else if (valorMoedaSelect === "2") {
-      bitcont = (valorInputSelect / 200000).toFixed(6);
-    } else if (valorMoedaSelect === "3") {
-      bitcont = (valorInputSelect / 300000).toFixed(6);
+    function handleChange(event) {
+      const valorMoeda = event.target.value;
+      const valorFinal = (valorMoeda / bitcont).toFixed(6);
+      valorCompra.innerHTML = `<span data-simbolo>$</span>${valorFinal}`;
     }
-  }
-  valorFinal();
-  valorCompra.innerHTML = `<span data-simbolo>$</span>${bitcont}`;
-  console.log(bitcont, valorInputSelect);
-}
+
+    async function handleChangeSelect(event) {
+      const valorMoedaSelect = event.target.value;
+      const valorInputSelect = +valorName.value;
+      function valorFinal() {
+        if (valorMoedaSelect === "1") {
+          bitcont = (valorInputSelect / +bitcontValor.USD.sell).toFixed(6);
+        } else if (valorMoedaSelect === "2") {
+          bitcont = (valorInputSelect / +bitcontValor.EUR.sell).toFixed(6);
+        } else if (valorMoedaSelect === "3") {
+          bitcont = (valorInputSelect / +bitcontValor.BRL.sell).toFixed(6);
+        }
+      }
+      valorFinal();
+      valorCompra.innerHTML = `<span data-simbolo>$</span>${bitcont}`;
+      console.log(bitcont, valorInputSelect);
+    }
+  });
